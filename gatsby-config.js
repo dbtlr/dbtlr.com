064813a059f61ed19689c29504c035ca5f5b2fc5
Drew Butler <hi@dbtlr.com>
2019-07-05 22:14:08 -0400

    Add GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-2253352-22`,
       },
     },
     `gatsby-plugin-feed`,
