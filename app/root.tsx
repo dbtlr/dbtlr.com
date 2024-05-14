7527b34b1b2aa170a2090552fe842d0e7aadff28
Drew Butler <hi@dbtlr.com>
2024-05-14 15:44:05 -0700

    Get it working with tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/inter';
+
+import { LinksFunction } from '@remix-run/cloudflare';
 import {
   Links,
   Meta,
@@ -5,6 +8,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+
+import stylesheet from '~/styles/index.css?url';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,34 +2,18 @@ import type { MetaFunction } from '@remix-run/cloudflare';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
+    { title: 'Drew Butler' },
     {
       name: 'description',
-      content: 'Welcome to Remix! Using Vite and Cloudflare!',
+      content: 'Drew Butler',
     },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Drew Butler</h1>
     </div>
   );
 }
