69a481eb53a58a0cacd4a0a6518e217884953067
Drew Butler <hi@dbtlr.com>
2024-05-14 21:35:18 -0700

    Update footer styles

--- app/components/PageFooter.tsx
+++ app/components/PageFooter.tsx
@@ -1,11 +1,14 @@
 export function PageFooter() {
   return (
-    <footer className="border-t border-gray-600 pt-5">
-      Copyright &copy; 2024 Drew Butler. Built with{' '}
-      <span className="material-symbols-outlined text-xs text-accent">
-        favorite
-      </span>{' '}
-      in Brooklyn, NY.
+    <footer className="flex flex-col gap-2 border-t border-gray-600 pt-5 text-center md:flex-row md:items-end">
+      <div>Copyright &copy; 2024 Drew Butler.</div>
+      <div>
+        Built with{' '}
+        <span className="material-symbols-outlined text-xs text-accent">
+          favorite
+        </span>{' '}
+        in Brooklyn, NY.
+      </div>
     </footer>
   );
 }
